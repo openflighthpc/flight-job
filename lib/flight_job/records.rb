0f3d1e2ba674dcc8aff16643104fff03c4d7819a
William McCumsite <william.mccumstie@gmail.com>
2021-03-02 19:07:55 +0000

    Ensure the format spec is supported

--- lib/flight_job/records.rb
+++ lib/flight_job/records.rb
@@ -83,6 +83,34 @@ module FlightJob
       }
     })
 
+    FORMAT_SCHEMA = JSONSchemer.schema({
+      "type" => "object",
+      "required" => ["type"],
+      "properties" => {
+        # NOTE: This is the field named "type" not a specification
+        "type" => { "type" => "string", "enum" => ["select", "text", "multiline_text"] }
+      },
+      "if" => { "properties" => { "type" => { "const" => "select" } } },
+      "then" => {
+        "required" => ["type", "options"],
+        "properties" => {
+          "options" => {
+            "type" => "array",
+            "items" => {
+              "type" => "object",
+              "additionalProperties" => false,
+              "required" => ["text", "value"],
+              "properties" => {
+                "text" => { "type" => "string" },
+                "value" => { "type" => "string" }
+              }
+            }
+          }
+        }
+      },
+      "else" => { "required" => ["type"] }
+    })
+
     attributes :text, :default, :format, :askWhen
 
     def related_question_id
@@ -94,6 +122,11 @@ module FlightJob
       if askWhen && !(ask_errors = ASK_WHEN_SCHEMA.validate(askWhen).to_a).empty?
         Config::CACHE.logger.error("Unsupported askWhen for question: #{id}")
         Config::CACHE.logger.debug(JSON.pretty_generate(ask_errors))
+        return false
+      end
+      unless (format_errors = FORMAT_SCHEMA.validate(format).to_a).empty?
+        Config::CACHE.logger.error("Unsupported format for question: #{id}")
+        Config::CACHE.logger.debug(JSON.pretty_generate(format_errors))
         return false
       end
       true
