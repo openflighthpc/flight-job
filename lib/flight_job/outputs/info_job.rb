5ebdc28354db663f531a23fc284e8b397a72912b
William McCumsite <william.mccumstie@gmail.com>
2021-03-10 15:15:08 +0000

    Display the standard output/error for the job

--- lib/flight_job/outputs/info_job.rb
+++ lib/flight_job/outputs/info_job.rb
@@ -31,35 +31,64 @@ module FlightJob
   module Outputs::InfoJob
     extend OutputMode::TLDR::Show
 
-    register_attribute(header: 'ID') { |j| j.id }
-    register_attribute(header: 'Script ID') { |j| j.script_id }
-    register_attribute(header: 'Submitted') { |j| j.submit_status == 0 }
-    register_attribute(header: 'State') { |j| j.state }
+    TEMPLATE = <<~ERB
+      <% each(:main) do |value, field:, padding:, **_| -%>
+      <%= padding -%><%= pastel.blue.bold field -%><%= pastel.bold ':' -%> <%= pastel.green value %>
+      <% end -%>
+      <%
+        submit_outputs = output.procs.select { |proc| proc.config[:section] == :submit }
+        unless submit_outputs.empty?
+      -%>
+
+      <%= pastel.blue.bold "Submit Standard Out" -%><%= pastel.bold ':' %>
+      <%= pastel.green submit_outputs.first.call(model) %>
+      <%= pastel.blue.bold "Submit Standard Error" -%><%= pastel.bold ':' %>
+      <%= pastel.green submit_outputs.last.call(model) %>
+      <% end -%>
+    ERB
+
+    register_attribute(section: :main, header: 'ID') { |j| j.id }
+    register_attribute(section: :main, header: 'Script ID') { |j| j.script_id }
+    register_attribute(section: :main, header: 'Submitted') { |j| j.submit_status == 0 }
+    register_attribute(section: :main, header: 'State') { |j| j.state }
 
     # Toggle the format of the created at time
-    register_attribute(header: 'Created At', verbose: true) { |j| j.created_at }
-    register_attribute(header: 'Created At', verbose: false) do |job|
+    register_attribute(section: :main, header: 'Created At', verbose: true) { |j| j.created_at }
+    register_attribute(section: :main, header: 'Created At', verbose: false) do |job|
       DateTime.rfc3339(job.created_at).strftime('%d/%m %H:%M')
     end
 
     # NOTE: These could be the predicted times instead of the actual, consider
     # delineating the two
-    register_attribute(header: 'Start Time', verbose: true) { |j| j.start_time }
-    register_attribute(header: 'Start Time', verbose: false) do |job|
+    register_attribute(section: :main, header: 'Start Time', verbose: true) { |j| j.start_time }
+    register_attribute(section: :main, header: 'Start Time', verbose: false) do |job|
       next nil unless job.start_time
       DateTime.rfc3339(job.start_time).strftime('%d/%m %H:%M')
     end
-    register_attribute(header: 'End Time', verbose: true) { |j| j.end_time }
-    register_attribute(header: 'End Time', verbose: false) do |job|
+    register_attribute(section: :main, header: 'End Time', verbose: true) { |j| j.end_time }
+    register_attribute(section: :main, header: 'End Time', verbose: false) do |job|
       next nil unless job.end_time
       DateTime.rfc3339(job.end_time).strftime('%d/%m %H:%M')
+    end
+
+    register_attribute(section: :submit, header: 'Submission STDOUT', verbose: true) do |job|
+      job.submit_stdout
+    end
+    register_attribute(section: :submit, header: 'Submission STOUT', verbose: true) do |job|
+      job.submit_stderr
     end
 
     def self.build_output(**opts)
       if opts.delete(:json)
         JSONRenderer.new(false, opts[:interactive])
       else
-        super(**opts)
+        super(template: TEMPLATE, **opts).tap do |output|
+          # OutputMode currently doesn't properly support escaping of the newlines
+          # due to how it has wrapped the underlying CSV library
+          if output.is_a? OutputMode::Outputs::Delimited
+            output.config.merge! write_converters: [->(f) { f.to_s.dump.sub(/\A"/, '').sub(/"\Z/, '') }]
+          end
+        end
       end
     end
   end
