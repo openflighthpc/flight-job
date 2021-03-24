ad4517bd7aa55db150402b329459b213bb130fd5
William McCumsite <william.mccumstie@gmail.com>
2021-03-24 11:39:53 +0000

    Bump the version to 2.1.0

--- lib/flight_job/version.rb
+++ lib/flight_job/version.rb
@@ -25,5 +25,5 @@
 # https://github.com/openflighthpc/flight-job
 #==============================================================================
 module FlightJob
-  VERSION = '2.0.0'
+  VERSION = '2.1.0'
 end
