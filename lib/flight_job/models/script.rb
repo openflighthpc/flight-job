8994ecc5cdfb2d632b4653888b65591d787ea894
William McCumsite <william.mccumstie@gmail.com>
2021-03-29 13:28:13 +0100

    Use an UUID as the internal_id for a script
    This will allow job's to uniquely reference the script
    even after being renamed

--- lib/flight_job/models/script.rb
+++ lib/flight_job/models/script.rb
@@ -28,6 +28,7 @@
 require 'json'
 require 'securerandom'
 require 'json_schemer'
+require 'pastel'
 
 require_relative '../render_context'
 
@@ -62,6 +63,10 @@ module FlightJob
 
     def self.metadata_path(id)
       File.join(FlightJob.config.scripts_dir, id, 'metadata.yaml')
+    end
+
+    def self.internal_id_path(public_id, internal_id)
+      File.join(FlightJob.config.scripts_dir, public_id, "internal-#{internal_id}")
     end
 
     validates :id, presence: true
@@ -118,7 +123,11 @@ module FlightJob
       end
     end
 
+    # TODO: Rename the id method to public_id
     attr_reader :id
+    def public_id
+      id
+    end
 
     def initialize(**opts)
       # Attempt to set the ID up front from the provided options
@@ -161,6 +170,41 @@ module FlightJob
       super(opts)
     end
 
+    # NOTE: This is the ID used to refer to the scripts from existing jobs. It
+    # is static to the lifetime of the script even after being renamed!
+    #
+    # PS: It will be inferred from the existing public_id for legacy scripts
+    def internal_id
+      @internal_id ||= begin
+        # Determine the internal_id from the path file
+        paths = Dir.glob(self.class.internal_id_path(public_id, '*')).sort
+
+        # Ensure there are no duplicates (this should not happen in practice)
+        if paths.length > 1
+          msg = <<~ERROR.chomp
+            Detected duplicate internal_ids for job '#{id}':
+            #{paths.join("\n")}
+
+            This may result in unusual/undefined behaviour of various commands!
+            Please remove all but one of the files to suppress this message.
+          ERROR
+          FlightJob.logger.error msg
+          $stderr.puts Pastel.new.red msg
+        end
+
+        # Determine the ID, generate a new one, or use the legacy public_id
+        if paths.empty? && exists?
+          # NOTE: This is for legacy scripts only (circa v2.0.0), consider removing
+          FileUtils.touch self.class.internal_id_path(public_id, public_id)
+          public_id
+        elsif paths.empty?
+          SecureRandom.uuid
+        else
+          File.basename(paths.first).split('-').last
+        end
+      end
+    end
+
     # Attempt to obtain the reservation for an ID
     # NOTE: The reservation is not checked at this point as the error handling
     # needs to be context specific. It does however get checked in the validation
@@ -200,14 +244,6 @@ module FlightJob
       return false
     end
 
-    # Used to reserve an ID before the script can be created. Only the process who's PID
-    # appears at the top of this list owns the ID. The reservation file becomes redundant
-    # once the `metadata` file exists.
-    def reservation_path
-      # NOTE: This path should not be cached
-      File.join(FlightJob.config.scripts_dir, id, 'reservation.pids')
-    end
-
     def metadata_path
       @metadata_path ||= self.class.metadata_path(id)
     end
@@ -221,6 +257,18 @@ module FlightJob
         @errors.add(:script_path, 'cannot be determined')
         @script_path = false
       end
+    end
+
+    # Used to reserve an ID before the script can be created. Only the process who's PID
+    # appears at the top of this list owns the ID. The reservation file becomes redundant
+    # once the `metadata` file exists.
+    def reservation_path
+      # NOTE: This path should not be cached
+      File.join(FlightJob.config.scripts_dir, id, 'reservation.pids')
+    end
+
+    def internal_id_path
+      @internal_id_path ||= self.class.internal_id_path(public_id, internal_id)
     end
 
     def created_at
@@ -291,8 +339,12 @@ module FlightJob
     def render_and_save(**answers)
       content = render(**answers)
 
-      # Writes the data to disk
+      # Create the internal_id tracking file
+      # NOTE: Must exist before the metadata file is created
       FileUtils.mkdir_p File.dirname(metadata_path)
+      FileUtils.touch internal_id_path
+
+      # Writes the data to disk
       File.write(metadata_path, YAML.dump(metadata))
       File.write(script_path, content)
       FileUtils.chmod(0700, script_path)
@@ -303,7 +355,9 @@ module FlightJob
 
     def serializable_hash
       {
-        "id" => id,
+        "id" => internal_id,
+        'internal_id' => internal_id,
+        'public_id' => public_id,
         "identity_name" => identity_name,
         "path" => script_path
       }.merge(metadata)
