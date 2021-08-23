1f8600bd10cbf0ee6dab4cf20d98989f646aa118
Ben Armston <ben.armston@alces-software.com>
2021-08-23 18:17:54 +0100

    Fix desktop id in info-job output

--- lib/flight_job/outputs/info_job.rb
+++ lib/flight_job/outputs/info_job.rb
@@ -125,7 +125,7 @@ module FlightJob
     end
 
     register_attribute(header: "Desktop ID:") do |job|
-      job.controls_file('flight-desktop-id').read
+      job.controls_file('flight_desktop_id').read
     end
 
     def self.build_output(**opts)
