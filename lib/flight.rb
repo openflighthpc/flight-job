924a763255f40ee7f317cdde096e3d89aacfbebd
Ben Armston <ben.armston@alces-software.com>
2021-09-14 12:34:01 +0100

    Change flight_ENVIRONMENT loading and defaults
    * `Flight.env` defaults to `standalone` if the `flight_ENVIRONMENT`
      environment variable is not set.
    * A single "dot env" file, now named `.flight-environment`, is now loaded.
      This value sets `flight_ENVIRONMENT` to `development`.
    * The omnibus builder is expected to ship its own `.flight-environment` file
      setting `flight_ENVIRONMENT` to `integrated`.
    * The values of `Flight.env` and `Flight.root` are logged once the logger is
      available.
    * `flight_ROOT` is only used for `integrated` environments.
    * If `flight_ENVIRONMENT` is `integrated` and `flight_ROOT` is not set an
      error is raised.

--- lib/flight.rb
+++ lib/flight.rb
@@ -39,6 +39,8 @@ module Flight
       return @config if @config
       @config = FlightJob::Configuration.build
       @config.tap do |c|
+        logger.info("Flight.env set to #{env.inspect}")
+        logger.info("Flight.root set to #{root.inspect}")
         c.__logs__.log_with(logger)
         raise FlightJob::ConfigError, c.rich_error_message unless c.valid?
       end
@@ -47,16 +49,19 @@ module Flight
 
     def env
       @env ||= ActiveSupport::StringInquirer.new(
-        ENV["flight_ENVIRONMENT"].presence || "production"
+        ENV["flight_ENVIRONMENT"].presence || "standalone"
       )
     end
 
     def root
-      @root ||= if env.production? && ENV["flight_ROOT"].present?
-        File.expand_path(ENV["flight_ROOT"])
-      else
-        File.expand_path('..', __dir__)
-      end
+      @root ||=
+        if env.production? && ENV["flight_ROOT"].present?
+          File.expand_path(ENV["flight_ROOT"])
+        elsif env.integrated? && !ENV["flight_ROOT"].present?
+          raise GeneralError, "flight_ROOT not set for integrated environment"
+        else
+          File.expand_path('..', __dir__)
+        end
     end
 
     def logger
