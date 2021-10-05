d60a013d9a5c60a5f88dcd67f511b01e80e3fe8d
William McCumsite <william.mccumstie@gmail.com>
2021-10-05 14:48:20 +0100

    Extract the register methods for Outputs::ListJobs

--- lib/flight_job/outputs/list_jobs.rb
+++ lib/flight_job/outputs/list_jobs.rb
@@ -29,14 +29,47 @@ require 'output_mode'
 
 module FlightJob
   class Outputs::ListJobs < OutputMode::Formatters::Index
-    constructor do
+    def register_id
       register(header: 'ID', row_color: :yellow) { |j| j.id }
+    end
+
+    def register_script_id
       register(header: 'Script ID') { |j| j.script_id }
+    end
+
+    def register_sched_id
+      register(header: 'Sched. ID') { |j| j.scheduler_id }
+    end
+
+    def register_state
+      register(header: 'State') { |j| j.state }
+    end
+
+    def register_shared_times
+      register(header: 'Submitted at', &:created_at)
+      register(header: 'Started at', &:actual_start_time)
+      register(header: 'Ended at', &:actual_end_time)
+    end
+
+    def register_paths
+      register(header: 'StdOut Path', &:stdout_path)
+      register(header: 'StdErr Path', &:stderr_path)
+      register(header: 'Results Dir', &:results_dir)
+    end
+
+    def register_estimated_times
+      register(header: 'Estimated Start',  &:estimated_start_time)
+      register(header: 'Estimated Finish', &:estimated_end_time)
+    end
+
+    constructor do
+      register_id
+      register_script_id
 
       if verbose?
-        register(header: 'Sched. ID') { |j| j.scheduler_id }
+        register_sched_id
       end
-      register(header: 'State') { |j| j.state }
+      register_state
 
       # Show a boolean in the "simplified" output, and the exit code in the verbose
       # NOTE: The headers are intentionally toggled between outputs
@@ -46,18 +79,11 @@ module FlightJob
         register(header: 'Submitted') { |j| j.submit_status == 0 }
       end
 
-      register(header: 'Submitted at', &:created_at)
-
-      register(header: 'Started at', &:actual_start_time)
-      register(header: 'Ended at', &:actual_end_time)
+      register_shared_times
 
       if verbose?
-        register(header: 'StdOut Path', &:stdout_path)
-        register(header: 'StdErr Path', &:stderr_path)
-        register(header: 'Results Dir', &:results_dir)
-
-        register(header: 'Estimated Start',  &:estimated_start_time)
-        register(header: 'Estimated Finish', &:estimated_end_time)
+        register_paths
+        register_estimated_times
       end
     end
   end
