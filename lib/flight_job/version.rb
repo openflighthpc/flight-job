a72b765b9450e52c518828ec674f27186554d523
Ben Armston <ben.armston@alces-software.com>
2021-10-27 17:37:58 +0100

    Bump version

--- lib/flight_job/version.rb
+++ lib/flight_job/version.rb
@@ -25,5 +25,5 @@
 # https://github.com/openflighthpc/flight-job
 #==============================================================================
 module FlightJob
-  VERSION = '2.5.0'
+  VERSION = '2.6.0-rc1'
 end
