53e1d872b3e8f7c7858e34f23059386a8205c033
Ben Armston <ben.armston@alces-software.com>
2022-03-10 13:45:45 +0000

    Extract schema defs from template file
    Perhaps later these can be migrated to a YAML or JSON file.  But for
    now, not having them in template.rb is a big win.

--- lib/flight_job/models/template.rb
+++ lib/flight_job/models/template.rb
@@ -25,336 +25,42 @@
 # https://github.com/openflighthpc/flight-job
 #==============================================================================
 
-require 'json_schemer'
+require "json_schemer"
 
-require_relative 'template/validator'
+require_relative "template/validator"
+require_relative "template/schema_defs"
 
 module FlightJob
   class Template < ApplicationModel
-    VALIDATOR_DEF = {
-      "validator_def" => {
-        "$comment" => "strip-schema",
-        "type" => "object",
-        "additionalProperties" => true,
-        "required" => ["type"],
-        "properties" => {
-          # The following keys are properties, however they deliberately partially
-          # shadows the JSON:Schema type
-          # NOTE: All the types must have a oneOf entry!
-          #
-          # NOTE: array must be last
-          "type" => { "enum" => ["string", "number", "integer", "boolean", "array"] },
-        },
-        "oneOf" => [
-          # String validators
-          {
-            "type" => "object",
-            "additionalProperties" => false,
-            "properties" => {
-              "type" => { "const" => "string" },
-              "required" => { "type" => "boolean" },
-              "pattern" => { "type" => "string", "format" => "regex" },
-              "pattern_error" => { "type" => "string" },
-              "enum" => { "type" => "array", "items" => { "type" => "string" }, "minItems" => 1 }
-            },
-          },
-          # Number validators
-          {
-            "type" => "object",
-            "additionalProperties" => false,
-            "properties" => {
-              "type" => { "const" => "number" },
-              "required" => { "type" => "boolean" },
-              "enum" => { "type" => "array", "items" => { "type" => ["number", "integer"], "minItems" => 1 } },
-              "minimum" => { "type" => ["number", "integer"] },
-              "maximum" => { "type" => ["number", "integer"] },
-              "exclusive_minimum" => { "type" => ["number", "integer"] },
-              "exclusive_maximum" => { "type" => ["number", "integer"] }
-            }
-          },
-          # Integer validators
-          {
-            "type" => "object",
-            "additionalProperties" => false,
-            "properties" => {
-              "type" => { "const" => "integer" },
-              "required" => { "type" => "boolean" },
-              "enum" => { "type" => "array", "items" => { "type" => "integer", "minItems" => 1 } },
-              "minimum" => { "type" => "integer" },
-              "maximum" => { "type" => "integer" },
-              "exclusive_minimum" => { "type" => "integer" },
-              "exclusive_maximum" => { "type" => "integer" }
-            }
-          },
-          # Boolean validator
-          {
-            "type" => "object",
-            "additionalProperties" => false,
-            "properties" => {
-              "type" => { "const" => "boolean" },
-              "required" => { "type" => "boolean" }
-            }
-          },
-          # Array validator
-          # NOTE: Must be last
-          {
-            "type" => "object",
-            "additionalProperties" => false,
-            "properties" => {
-              "type" => { "const" => "array" },
-              "required" => { "type" => "boolean" },
-              "items" => { "$ref" => "#/$defs/array_validator_def" }
-            }
-          }
-        ]
-      }
-    }
-
-    # Prevent deeply nested arrays
-    VALIDATOR_DEF["array_validator_def"] = VALIDATOR_DEF["validator_def"].deep_dup
-    VALIDATOR_DEF["array_validator_def"]["properties"]["type"]["enum"].delete("array")
-    VALIDATOR_DEF["array_validator_def"]["oneOf"].delete_if { |s| s["properties"]["type"]["const"] == "array" }
-
-    DYNAMIC_DEFAULT_SPEC = {
-      "type" => "object",
-      "required" => ["type"],
-      "properties" => {
-        # The following is a field called "type".
-        "type" => {
-          "type" => "string" ,
-          "enum" => [
-            "path_placeholder"
-          ]
-        }
-      }
-    }
-
-    DYNAMIC_OPTIONS_SPEC = {
-      "type" => "object",
-      "required" => ['type'],
-      "properties" => {
-        "type" => {
-          "type" => "string" ,
-          "enum" => [
-            "file_listing"
-          ]
-        },
-        "format_path" => { "enum" => ["absolute", "relative", "basename" ] },
-        "glob" => { "type" => "string" },
-        "include_null" => { "type" => { "oneof" => ["boolean", "string"] } },
-        "directories" => {
-          "type" => "array",
-          "items" => {
-            "type" => "string",
-          }
-        }
-      }
-    }
-
-    ASK_WHEN_SPEC = {
-      "type" => "object",
-      "additionalProperties" => false,
-      "required" => ['value', 'eq'],
-      "properties" => {
-        # NOTE: The question asking mechanism is coupled to the pattern match
-        # bellow. They must be updated in tandem
-        'value' => { "type" => "string", "pattern" => "^question\.[a-zA-Z_-]+\.answer$" },
-        'eq' => { "type" => "string" }
-      }
-    }
-
-    QUESTION_PROPS_STUB = {
-      "id" => {}, "text" => {}, "description" => {}, "dynamic_default" => {},
-      "validate" => {}, "ask_when" => {}
-    }
-    QUESTION_DEF = {
-      "question_def" => {
-        # Generic top level definition of each format
-        "$comment" => "strip-schema",
-        "type" => "object",
-        "additionalProperties" => true,
-        "required" => ["id", "text", "format"],
-        "properties" => {
-          'id' => { 'type' => 'string' },
-          'text' => { 'type' => 'string' },
-          "description" => { "type" => "string" },
-          'validate' => { "$ref" => "#/$defs/validator_def" },
-          'ask_when' => ASK_WHEN_SPEC,
-          "dynamic_default" => DYNAMIC_DEFAULT_SPEC,
-          "format" => {
-            "type" => "object",
-            "required" => ["type"],
-            "additionalProperties" => true,
-            "properties" => {
-              'type' => {
-                "enum" => [
-                  "text", "time", "select", "multiselect", 'multiline_text', 'number'
-                ]
-              }
-            }
-          }
-        },
-
-        "oneOf" => [
-          # (Multi) Text format questions
-          {
-            "type" => "object",
-            "additionalProperties" => false,
-            "properties" => {
-              **QUESTION_PROPS_STUB,
-              "default" => { "type" => "string" },
-              "format" => {
-                "type" => "object",
-                "additionalProperties" => false,
-                "properties" => {
-                  "type" => { "const" => "text" }
-                }
-              }
-            }
-          },
-
-          # Multi Text format questions
-          {
-            "type" => "object",
-            "additionalProperties" => false,
-            "properties" => {
-              **QUESTION_PROPS_STUB,
-              "default" => { "type" => "string" },
-              "format" => {
-                "type" => "object",
-                "additionalProperties" => false,
-                "properties" => {
-                  "type" => { "const" => "multiline_text" }
-                }
-              }
-            }
-          },
-
-          # Time format questions
-          {
-            "type" => "object",
-            "additionalProperties" => false,
-            "properties" => {
-              **QUESTION_PROPS_STUB,
-              # XXX: The default needs to be hardened for this
-              "default" => {},
-              "format" => {
-                "type" => "object",
-                "additionalProperties" => false,
-                "properties" => {
-                  "type" => { "const" => "time" }
-                }
-              }
-            }
-          },
-
-          # Number format questions
-          # NOTE: By default, HTML <number> only supports integers
-          {
-            "type" => "object",
-            "additionalProperties" => false,
-            "properties" => {
-              **QUESTION_PROPS_STUB,
-              "default" => { "type" => "integer" },
-              "format" => {
-                "type" => "object",
-                "additionalProperties" => false,
-                "properties" => {
-                  "type" => { "const" => "number" }
-                }
-              }
-            }
-          },
-
-          # Select format questions
-          {
-            "type" => "object",
-            "additionalProperties" => false,
-            "properties" => {
-              **QUESTION_PROPS_STUB,
-              "default" => { "type" => ["string", "integer", "number"] },
-              "format" => {
-                "type" => "object",
-                "additionalProperties" => false,
-                "properties" => {
-                  "type" => { "const" => "select" },
-                  "options" => {
-                    "type" => "array",
-                    "items" => {
-                      "type" => "object",
-                      "additionalProperties" => false,
-                      "required" => ["text", "value"],
-                      "properties" => {
-                        "text" => { "type" => "string" },
-                        "value" => { "type" => ["string", "integer", "number"] }
-                      }
-                    }
-                  },
-                  "dynamic_options" => DYNAMIC_OPTIONS_SPEC,
-                }
-              }
-            }
-          },
-
-          # Multi-select format questions
-          {
-            "type" => "object",
-            "additionalProperties" => false,
-            "properties" => {
-              **QUESTION_PROPS_STUB,
-              "default" => {
-                "type" => "array",
-                "items" => { "type" => ["string", "integer", "number"] },
-              },
-              "format" => {
-                "type" => "object",
-                "additionalProperties" => false,
-                "properties" => {
-                  "type" => { "const" => "multiselect" },
-                  "options" => {
-                    "type" => "array",
-                    "items" => {
-                      "type" => "object",
-                      "additionalProperties" => false,
-                      "required" => ["text", "value"],
-                      "properties" => {
-                        "text" => { "type" => "string" },
-                        "value" => { "type" => ["string", "integer", "number"] }
-                      }
-                    }
-                  },
-                  "dynamic_options" => DYNAMIC_OPTIONS_SPEC,
-                }
-              }
-            }
-          }
-        ]
-      }
-    }
-
     SCHEMA = JSONSchemer.schema({
       "type" => "object",
       "$comment" => "strip-schema",
       "additionalProperties" => false,
-      "required" => ['synopsis', 'version', 'generation_questions', 'name', 'copyright', 'license'],
+      "required" => [
+        "synopsis",
+        "version",
+        "generation_questions",
+        "name",
+        "copyright",
+        "license"
+      ],
       "properties" => {
-        'copyright' => { "type" => "string" },
-        'description' => { "type" => 'string' },
-        'generation_questions' => {
+        "copyright" => { "type" => "string" },
+        "description" => { "type" => "string" },
+        "generation_questions" => {
           "type" => "array",
           "items" => { "$ref" => "#/$defs/question_def" }
         },
-        'license' => { "type" => "string" },
-        'name' => { "type" => 'string' },
-        'priority' => { "type" => 'integer' },
-        'script_template' => { "type" => 'string' },
-        'synopsis' => { "type" => 'string' },
-        'tags' => { "type" => 'array', 'items' => { 'type' => 'string' }},
-        'version' => { "type" => 'integer', 'enum' => [0] },
-        '__meta__' => {}
+        "license" => { "type" => "string" },
+        "name" => { "type" => "string" },
+        "priority" => { "type" => "integer" },
+        "script_template" => { "type" => "string" },
+        "synopsis" => { "type" => "string" },
+        "tags" => { "type" => "array", "items" => { "type" => "string" }},
+        "version" => { "type" => "integer", "enum" => [0] },
+        "__meta__" => {}
       },
-      "$defs" => {}.merge!(VALIDATOR_DEF)
-                   .merge!(QUESTION_DEF)
+      "$defs" => {}.merge!(SchemaDefs::VALIDATOR_DEF).merge!(SchemaDefs::QUESTION_DEF)
     })
 
     def self.load_all
