b3629fc210689e2b8b73a87ab4827297bcf0f9a9
megan-godfrey <megan.godfrey@alces-software.com>
2022-05-03 17:23:21 +0100

    Class not found error - NameError: uninitialized constant FlightJob::Script::ScriptMetadata

---
+++ lib/flight_job/models/script/metadata.rb
@@ -0,0 +1,50 @@
+#==============================================================================
+# Copyright (C) 2022-present Alces Flight Ltd.
+#
+# This file is part of Flight Job.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# Flight Job is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with Flight Job. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on Flight Job, please visit:
+# https://github.com/openflighthpc/flight-job
+#==============================================================================
+
+#require_relative "validator"
+
+module FlightJob
+  class Script < ApplicationModel
+    # Encapsulates a script's `metadata.yaml` file.
+    #
+    # * Loads and saves the file.
+    # * Validates against a schema.
+    # * Provides "atomic"(-ish) updates.
+    class ScriptMetadata < Metadata::BaseMetadata
+
+      def self.from_template(script)
+        initial_metadata = {
+          "created_at" => Time.now.rfc3339,
+        }
+        path = File.join(script.metadata_path, "metadata.yaml")
+        new(initial_metadata, path, script)
+      end
+
+
+    end
+  end
+end
