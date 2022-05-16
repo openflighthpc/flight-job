a7dffe1f87cc4a4f45eb19bbc5490bd38d90a540
megan-godfrey <102584263+megan-godfrey@users.noreply.github.com>
2022-05-16 11:29:49 +0100

    Added notes contents check to script_spec

--- spec/models/script_spec.rb
+++ spec/models/script_spec.rb
@@ -2,51 +2,57 @@ require 'flight_job_helper'
 
 RSpec.describe "FlightJob::Script", type: :model do
   let(:config) { FlightJob.config }
+  let(:metadata) { YAML.load_file(metadata_path) }
   let(:metadata_path) { File.join(script_dir,"metadata.yaml") }
+  let(:script_name) { "interactive-desktop.sh" }
   let(:script_dir) { File.join(config.scripts_dir, script_id) }
   let(:template_id) { "desktop-on-login-node" }
   let(:template) { FlightJob::Template.new(id: template_id) }
 
   describe "script creation" do
     let(:script_id) { "new-script" }
-    let(:script_name) { "interactive-desktop.sh" }
+    let(:test_notes) { "test notes" }
     let(:notes_path) { File.join(script_dir,"notes.md") }
     let(:job_script_path) { File.join(script_dir,"script.sh") }
 
-    it "writes the notes" do
-      check_for_file_creation(notes_path)
-    end
-
-    it "writes the job script file" do
-      check_for_file_creation(job_script_path)
-    end
-
-    it "writes the metadata" do
-      check_for_file_creation(metadata_path)
-    end
-  end
-
-  def check_for_file_creation(file_path)
-    FakeFS.with_fresh do
-      FakeFS::FileSystem.clone(config.templates_dir)
-      FakeFS::FileSystem.clone(config.adapter_script_path)
-      FakeFS::FileSystem.clone("/tmp/bundle/ruby/2.7.0/gems/activesupport-6.1.3")
-      FakeFS::FileSystem.clone("/tmp/bundle/ruby/2.7.0/gems/activemodel-6.1.3")
-
-      expect(File).not_to exist(file_path)
-      FlightJob::Script.new( id: script_id ).tap do |s|
-        s.initialize_metadata(template, {})
-        s.render_and_save
+    context "writes the notes" do
+      it "creates the notes file" do
+        check_for_file_creation(notes_path)
       end
-      expect(File).to exist(file_path)
+      it "saves the notes correctly" do
+        fresh_fakefs do
+          create_and_save_script(notes: test_notes)
+          new_script = FlightJob::Script.new(id: script_id)
+          expect(new_script.notes).to eq(test_notes)
+        end
+      end
+    end
+
+    context "creates the job script" do
+      it "writes the job script file" do
+        check_for_file_creation(job_script_path)
+      end
+    end
+
+    context "writes the metadata" do
+      it "creates the metadata file" do
+        check_for_file_creation(metadata_path)
+      end
+      it "saves the metadata correctly" do
+        fresh_fakefs do
+          create_and_save_script
+          new_script = FlightJob::Script.new(id: script_id)
+
+          %w(created_at script_name tags template_id).each do |attr|
+            expect(new_script.send(attr)).to eq(metadata[attr])
+          end
+        end
+      end
     end
   end
-
 
   describe "metadata" do
     let(:script_id) { "valid-script" }
-    let(:metadata) { YAML.load_file(metadata_path) }
-
     subject(:script) { FlightJob::Script.new(id: script_id) }
 
     it "has the expected metadata path" do
@@ -74,22 +80,28 @@ RSpec.describe "FlightJob::Script", type: :model do
         expect(script.send(attr)).to eq(value)
       end
     end
+  end
 
-    it "writes the file contents correctly" do
-      FakeFS.with_fresh do
-        FakeFS::FileSystem.clone(config.templates_dir)
-        FakeFS::FileSystem.clone(config.adapter_script_path)
+  def check_for_file_creation(file_path)
+    fresh_fakefs do
+      expect(File).not_to exist(file_path)
+      create_and_save_script
+      expect(File).to exist(file_path)
+    end
+  end
 
-        FlightJob::Script.new(id: script_id).tap do |s|
-          s.initialize_metadata(template, {})
-          s.render_and_save
-        end
-        new_script = FlightJob::Script.new(id: script_id)
+  def create_and_save_script(id: script_id, notes: nil)
+    FlightJob::Script.new( id: id, notes: notes ).tap do |s|
+      s.initialize_metadata(template, {})
+      s.render_and_save
+    end
+  end
 
-        %w(created_at script_name tags template_id).each do |attr|
-          expect(new_script.send(attr)).to eq(metadata[attr])
-        end
-      end
+  def fresh_fakefs
+    FakeFS.with_fresh do
+      FakeFS::FileSystem.clone(config.templates_dir)
+      FakeFS::FileSystem.clone(config.adapter_script_path)
+      yield
     end
   end
 end
